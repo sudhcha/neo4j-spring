b7e983c75e49885d985f371f312443fa17d90bb8
Sudhin Varghese <sudh_chandy@yahoo.com>
2012-08-18 04:44:52 -0400

    Fix test

--- src/main/java/org/sudhcha/spring/neo4j/Movie.java
+++ src/main/java/org/sudhcha/spring/neo4j/Movie.java
@@ -20,6 +20,11 @@ public class Movie {
 	
 	int year;
 	
+	public Movie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 	public Movie(String title, int year) {
 		super();
 		this.title = title;
@@ -48,6 +53,34 @@ public class Movie {
 
 	public void setYear(int year) {
 		this.year = year;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + year;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
 	}
 
 	
